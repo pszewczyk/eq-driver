51af03662c5a88b557d68f77aa4b63c90c6fe98f
root <root@localhost.localdomain>
2017-01-24 16:07:39 +0000

    Add basics of BLE stack initialization
    Need additional sdk sources for compilation
    
    Signed-off-by: root <root@localhost.localdomain>

--- nrf51/main.c
+++ nrf51/main.c
@@ -1,15 +1,50 @@
 #include "nrf.h"
 #include "nrf_gpio.h"
 #include "nrf_delay.h"
+#include "ble.h"
+#include "softdevice_handler.h"
 
 #define LED_PIN 21
 
+void app_error_fault_handler(uint32_t id, uint32_t pc, uint32_t info)
+{
+	/* Fault handler stub */
+}
+
+/** @brief Initialize ble stack */
+static int ble_init()
+{
+	int ret;
+	ble_enable_params_t enable_params;
+
+	/* Low frequency clock settings */
+	nrf_clock_lf_cfg_t nrf_clock = {.source = NRF_CLOCK_LF_SRC_XTAL,
+                                 .rc_ctiv       = 0,
+                                 .rc_temp_ctiv  = 0,
+                                 .xtal_accuracy = NRF_CLOCK_LF_XTAL_ACCURACY_20_PPM};
+
+	SOFTDEVICE_HANDLER_INIT(&nrf_clock, NULL);
+
+	ret = softdevice_enable_get_default_config(0, 1, &enable_params);
+	if (ret < 0)
+		return ret;
+
+	/* Enable or disable service changed characteristics */
+	enable_params.gatts_enable_params.service_changed = 0;
+	ret = softdevice_enable(&enable_params);
+	if (ret < 0)
+		return ret;
+
+	return 0;
+}
+
 int main(void)
 {
-    nrf_gpio_pin_dir_set(LED_PIN, NRF_GPIO_PIN_DIR_OUTPUT);
-    while (1)
-    {
-    	nrf_gpio_pin_toggle(LED_PIN);
-	nrf_delay_ms(100);
-    }
+	ble_init();
+	nrf_gpio_pin_dir_set(LED_PIN, NRF_GPIO_PIN_DIR_OUTPUT);
+	while (1)
+	{
+		nrf_gpio_pin_toggle(LED_PIN);
+		nrf_delay_ms(500);
+	}
 }
