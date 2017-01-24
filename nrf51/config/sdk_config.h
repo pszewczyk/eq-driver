1b43f5e0e45084c9679cce0cdda3c9ffd8c54750
root <root@localhost.localdomain>
2017-01-24 18:10:51 +0000

    Enable required modules and add their sources
    Actually add all sources, ifdefs take care of unsed ones.
    
    Signed-off-by: root <root@localhost.localdomain>

--- nrf51/config/sdk_config.h
+++ nrf51/config/sdk_config.h
@@ -1,6 +1,14 @@
 #ifndef SDK_CONFIG_H
 #define SDK_CONFIG_H
 
-/* stub */
+#define BLE_ADVERTISING_ENABLED 1
+#define FSTORAGE_ENABLED 1
+
+/* FSTORAGE values from template file in sdk */
+#define FS_QUEUE_SIZE 4 /**< the size of the internal queue */
+#define FS_MAX_WRITE_SIZE_WORDS 256 /**< Maximum number of words to be written to flash in a single operation. */
+#define FS_OP_MAX_RETRIES 3 /**< Number attempts to execute an operation if the SoftDevice fails */
+
+#define APP_TIMER_ENABLED 1
 
 #endif
--- nrf51/main.c
+++ nrf51/main.c
@@ -3,12 +3,36 @@
 #include "nrf_delay.h"
 #include "ble.h"
 #include "softdevice_handler.h"
+#include "ble_conn_params.h"
+#include "ble_advertising.h"
 
 #define LED_PIN 21
 
 void app_error_fault_handler(uint32_t id, uint32_t pc, uint32_t info)
 {
 	/* Fault handler stub */
+}
+
+static void app_on_ble_evt(ble_evt_t *ble_evt)
+{
+}
+
+static void ble_event_handler(ble_evt_t *ble_evt)
+{
+	/* This handler can call already implemented handlers from used modules */
+	/* Connection handler */
+	ble_conn_params_on_ble_evt(ble_evt);
+	
+	/* Our application handler */
+	app_on_ble_evt(ble_evt);
+
+	/* Advertising handler */
+	ble_advertising_on_ble_evt(ble_evt);
+}
+
+static void sys_event_handler(uint32_t sys_evt)
+{
+	ble_advertising_on_sys_evt(sys_evt);
 }
 
 /** @brief Initialize ble stack */
@@ -32,6 +56,14 @@ static int ble_init()
 	/* Enable or disable service changed characteristics */
 	enable_params.gatts_enable_params.service_changed = 0;
 	ret = softdevice_enable(&enable_params);
+	if (ret < 0)
+		return ret;
+
+	ret = softdevice_ble_evt_handler_set(ble_event_handler);
+	if (ret < 0)
+		return ret;
+
+	ret = softdevice_sys_evt_handler_set(sys_event_handler);
 	if (ret < 0)
 		return ret;
 
