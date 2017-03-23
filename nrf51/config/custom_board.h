7d109ebcf73cc9ae69378dc1cbf557348390bd4b
Pawel Szewczyk <pfpawelec@gmail.com>
2017-03-23 22:31:53 +0000

    Add BLE400 pinout definition
    BLE400 have some useful features, probably the pinout will be copied in final board design.
    
    Signed-off-by: Pawel Szewczyk <pfpawelec@gmail.com>

--- nrf51/config/custom_board.h
+++ nrf51/config/custom_board.h
@@ -1,1 +1,16 @@
 /* This is custom board definition */
+
+/* BLE400 board has some useful features like keys and leds */
+#define KEY1 16
+#define KEY2 17
+
+#define LED0 18
+#define LED1 19
+#define LED2 20
+#define LED3 21
+#define LED4 22
+
+#define CPCTS 8
+#define CPRTS 10
+#define CPRX 9
+#define CPTX 11
