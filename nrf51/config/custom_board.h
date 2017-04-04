2d66594dec51f782b2861001615ad65430dacc93
Pawel Szewczyk <pfpawelec@gmail.com>
2017-04-04 17:59:20 +0200

    Add motor-related timers
    Signed-off-by: Pawel Szewczyk <pfpawelec@gmail.com>

--- nrf51/config/custom_board.h
+++ nrf51/config/custom_board.h
@@ -1,9 +1,10 @@
 /* This is custom board definition */
 
 /* BLE400 board has some useful features like keys and leds */
+
 #define KEY1 16
 #define KEY2 17
-
+ 
 #define LED0 18
 #define LED1 19
 #define LED2 20
@@ -14,3 +15,17 @@
 #define CPRTS 10
 #define CPRX 9
 #define CPTX 11
+
+#define RA_EN 0
+#define RA_DIR 30
+#define RA_STEP 1
+#define RA_MS1 2
+#define RA_MS2 4
+#define RA_MS3 3
+
+#define DEC_EN 10
+#define DEC_DIR 9
+#define DEC_STEP 7
+#define DEC_MS1 8
+#define DEC_MS2 6
+#define DEC_MS3 5
--- nrf51/main.c
+++ nrf51/main.c
@@ -13,13 +13,41 @@
 #define LED_PIN 21
 
 #define SERVICE_UUID 0x1523
+#define MOTOR_STEP_RATIO 200
+#define MOTOR_MICROSTEPS 16
+#define GEAR_RATIO 130
+#define STELLAR_DAY 86164
+#define GOTO_ACCURACY 60
 
+/* nuber of clock ticks per one motor step for tracking */
+#define STEP_TICKS (APP_TIMER_CLOCK_FREQ / MOTOR_MICROSTEPS) * (STELLAR_DAY / (MOTOR_STEP_RATIO * GEAR_RATIO))
+
+/* step time for goto */
+#define GOTO_STEP_MS 2
+
+/* length of single microstep in arc seconds */
+#define STEP_LENGTH (360*60*60)/(MOTOR_STEP_RATIO * GEAR_RATIO * MOTOR_MICROSTEPS)
+#define FULL_STEP_LENGTH (360*60*60)/(MOTOR_STEP_RATIO * GEAR_RATIO)
+
+enum {
+	MODE_TRACKING,
+	MODE_GOTO,
+	MODE_OFF,
+};
+
+#define CHAR_NUMBER 4
 struct context {
 	uint16_t service_handle;
-	uint32_t ra;
-	uint32_t dec;
-	ble_gatts_char_handles_t char_handle[2];
+	uint32_t pos[2]; /**< position in acrseconds {RA, DEC} */
+	uint32_t dest[2]; /**< destination in arcseconds {RA, DEC} */
+	uint8_t mode;
+	uint8_t reverse; /**< for simple reversing direction */
+	ble_gatts_char_handles_t char_handle[CHAR_NUMBER];
 } ctx;
+
+APP_TIMER_DEF(led_timer_id);
+APP_TIMER_DEF(sky_movement_timer_id);
+APP_TIMER_DEF(goto_timer_id);
 
 void app_error_fault_handler(uint32_t id, uint32_t pc, uint32_t info)
 {
@@ -30,8 +58,6 @@ void led_timer_handler(void *p_context)
 {
 	nrf_gpio_pin_toggle(LED0);
 }
-
-APP_TIMER_DEF(led_timer_id);
 
 /* dummy timer */
 void timer_init()
@@ -60,16 +86,177 @@ static int services_init()
 			&uuid, &ctx.service_handle);
 	APP_ERROR_CHECK(ret);
 
-	ctx.ra = 0xfefe;
-	ctx.dec = 0xbaba;
+/* TODO ERROR handling
+ * TODO UUID generalization*/
+	ret = eq_add_characteristic(ctx.service_handle, 2 * sizeof(uint32_t),
+			(uint8_t *)ctx.pos[0], 0x1525, PERM_WRITE | PERM_READ, &ctx.char_handle[0]);
 
-	ret = eq_add_characteristic(ctx.service_handle, sizeof(ctx.ra),
-			(uint8_t *)&ctx.ra, 0x1525, PERM_WRITE | PERM_READ, &ctx.char_handle[0]);
+	ret = eq_add_characteristic(ctx.service_handle, 2 * sizeof(uint32_t),
+			(uint8_t *)ctx.dest, 0x1526, PERM_WRITE | PERM_READ, &ctx.char_handle[1]);
 
-	ret = eq_add_characteristic(ctx.service_handle, sizeof(ctx.dec),
-			(uint8_t *)&ctx.dec, 0x1526, PERM_WRITE | PERM_READ, &ctx.char_handle[1]);
+	ret = eq_add_characteristic(ctx.service_handle, sizeof(ctx.mode),
+			(uint8_t *)&ctx.mode, 0x1528, PERM_WRITE | PERM_READ, &ctx.char_handle[2]);
 
-	return 0;
+	ret = eq_add_characteristic(ctx.service_handle, sizeof(ctx.reverse),
+			(uint8_t *)&ctx.reverse, 0x1528, PERM_WRITE | PERM_READ, &ctx.char_handle[3]);
+
+	return ret;
+}
+
+static void sky_movement_timer_handler(void *p_context)
+{
+	switch (ctx.mode) {
+	case MODE_TRACKING:
+		/* direction was already set */
+		nrf_gpio_pin_toggle(RA_STEP);
+		break;
+	case MODE_GOTO:
+		/* In GOTO mode RA motor is controlled by different routine. */
+		if (ctx.reverse)
+			ctx.dest[0] -= STEP_LENGTH;
+		else
+			ctx.dest[0] += STEP_LENGTH;
+		break;
+	}
+}
+
+enum {
+	DIR_PROGRADE,
+	DIR_RETROGRADE,
+};
+
+static void ra_set_dir(int dir)
+{
+	if (dir == DIR_PROGRADE || (dir == DIR_RETROGRADE && ctx.reverse))
+		nrf_gpio_pin_set(RA_DIR);
+	else
+		nrf_gpio_pin_clear(RA_DIR);
+}
+
+static void dec_set_dir(int dir)
+{
+	if (dir == DIR_PROGRADE || (dir == DIR_RETROGRADE && ctx.reverse))
+		nrf_gpio_pin_set(DEC_DIR);
+	else
+		nrf_gpio_pin_clear(DEC_DIR);
+}
+
+static void set_mode(int mode)
+{
+	int ret;
+
+	if (mode == ctx.mode)
+		return;
+
+	/* Stop previous mode */
+	switch (ctx.mode) {
+	case MODE_TRACKING:
+		break;
+	case MODE_GOTO:
+		app_timer_stop(goto_timer_id);
+		break;
+	}
+
+	switch (mode) {
+	case MODE_TRACKING:
+		/* 1/16 microsteps */
+		nrf_gpio_pin_set(RA_MS1);
+		nrf_gpio_pin_set(RA_MS2);
+		nrf_gpio_pin_set(RA_MS3);
+
+		nrf_gpio_pin_clear(RA_EN);
+		nrf_gpio_pin_clear(RA_DIR);
+
+		/* disable DEC motor */
+		nrf_gpio_pin_set(DEC_EN);
+
+		ra_set_dir(DIR_PROGRADE);
+
+		break;
+	case MODE_GOTO:
+		/* full steps for better speed */
+		nrf_gpio_pin_clear(RA_MS1);
+		nrf_gpio_pin_clear(RA_MS2);
+		nrf_gpio_pin_clear(RA_MS3);
+
+		ret = app_timer_start(goto_timer_id, APP_TIMER_TICKS(GOTO_STEP_MS, TIMER_PRESCALER), NULL);
+		APP_ERROR_CHECK(ret);
+		break;
+	case MODE_OFF:
+		break;
+	}
+
+	ctx.mode = mode;
+}
+
+static void goto_timer_handler(void *p_context)
+{
+	int end = 1;
+
+	/* ensure we should do anything */
+	if (ctx.mode != MODE_GOTO)
+		return;
+
+	if (ctx.dest[0] - ctx.pos[0] > GOTO_ACCURACY) {
+		ra_set_dir(DIR_PROGRADE);
+		nrf_gpio_pin_toggle(RA_STEP);
+		ctx.pos[0] += FULL_STEP_LENGTH;
+		end = 0;
+	} else if (ctx.pos[0] - ctx.dest[0] > GOTO_ACCURACY) {
+		ra_set_dir(DIR_RETROGRADE);
+		nrf_gpio_pin_toggle(RA_STEP);
+		ctx.pos[0] -= FULL_STEP_LENGTH;
+		end = 0;
+	}
+
+	if (ctx.dest[1] - ctx.pos[1] > GOTO_ACCURACY) {
+		dec_set_dir(DIR_PROGRADE);
+		nrf_gpio_pin_toggle(DEC_STEP);
+		ctx.pos[1] += FULL_STEP_LENGTH;
+		end = 0;
+	} else if (ctx.pos[1] - ctx.dest[1] > GOTO_ACCURACY) {
+		dec_set_dir(DIR_RETROGRADE);
+		nrf_gpio_pin_toggle(DEC_STEP);
+		ctx.pos[1] -= FULL_STEP_LENGTH;
+		end = 0;
+	}
+
+	/* return to tracking mode */
+	if (end)
+		set_mode(MODE_TRACKING);
+}
+
+static int motor_init()
+{
+	uint32_t ret;
+
+	/* sky moves always and this timer is always active */
+	ret = app_timer_create(&sky_movement_timer_id, APP_TIMER_MODE_REPEATED,
+			sky_movement_timer_handler);
+	APP_ERROR_CHECK(ret);
+
+	ret = app_timer_start(sky_movement_timer_id, STEP_TICKS, NULL);
+	APP_ERROR_CHECK(ret);
+
+	ret = app_timer_create(&goto_timer_id, APP_TIMER_MODE_REPEATED,
+			goto_timer_handler);
+	APP_ERROR_CHECK(ret);
+
+	nrf_gpio_pin_dir_set(RA_EN, NRF_GPIO_PIN_DIR_OUTPUT);
+	nrf_gpio_pin_dir_set(RA_DIR, NRF_GPIO_PIN_DIR_OUTPUT);
+	nrf_gpio_pin_dir_set(RA_STEP, NRF_GPIO_PIN_DIR_OUTPUT);
+	nrf_gpio_pin_dir_set(RA_MS1, NRF_GPIO_PIN_DIR_OUTPUT);
+	nrf_gpio_pin_dir_set(RA_MS2, NRF_GPIO_PIN_DIR_OUTPUT);
+	nrf_gpio_pin_dir_set(RA_MS3, NRF_GPIO_PIN_DIR_OUTPUT);
+
+	nrf_gpio_pin_dir_set(DEC_EN, NRF_GPIO_PIN_DIR_OUTPUT);
+	nrf_gpio_pin_dir_set(DEC_DIR, NRF_GPIO_PIN_DIR_OUTPUT);
+	nrf_gpio_pin_dir_set(DEC_STEP, NRF_GPIO_PIN_DIR_OUTPUT);
+	nrf_gpio_pin_dir_set(DEC_MS1, NRF_GPIO_PIN_DIR_OUTPUT);
+	nrf_gpio_pin_dir_set(DEC_MS2, NRF_GPIO_PIN_DIR_OUTPUT);
+	nrf_gpio_pin_dir_set(DEC_MS3, NRF_GPIO_PIN_DIR_OUTPUT);
+
+	return ret;
 }
 
 int main(void)
@@ -93,6 +280,9 @@ int main(void)
 	NRF_LOG_INFO("\r\nInitializing timers...\r\n")
 	timer_init();
 
+	NRF_LOG_INFO("\r\nInitializing motors...\r\n")
+	motor_init();
+
 	/* Starting bluetooth service */
 	ret = ble_advertising_start(BLE_ADV_MODE_FAST);
 	APP_ERROR_CHECK(ret);
@@ -101,9 +291,6 @@ int main(void)
 	nrf_gpio_pin_set(LED_PIN);
 
 	NRF_LOG_INFO("\r\nInitialization completed\r\n")
-
-	ctx.ra = 0x1337;
-	ctx.dec = 0xdeadbeef;
 
 	while (1)
 	{
