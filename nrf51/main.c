01b0706bc1ba0bf6f30b38bfdd4bd8852d2cfc16
Pawel Szewczyk <pfpawelec@gmail.com>
2017-04-05 23:06:25 +0200

    Add manual mode
    In manual mode user controls which way to go.
    TODO: Enable speed adjusting
    
    Signed-off-by: Pawel Szewczyk <pfpawelec@gmail.com>

--- nrf51/main.c
+++ nrf51/main.c
@@ -25,6 +25,9 @@
 /* step time for goto */
 #define GOTO_STEP_MS 2
 
+/* step time for manual control */
+#define MANUAL_STEP_MS 20
+
 /* length of single microstep in arc seconds */
 #define STEP_LENGTH (360*60*60)/(MOTOR_STEP_RATIO * GEAR_RATIO * MOTOR_MICROSTEPS)
 #define FULL_STEP_LENGTH (360*60*60)/(MOTOR_STEP_RATIO * GEAR_RATIO)
@@ -32,6 +35,7 @@
 enum {
 	MODE_TRACKING,
 	MODE_GOTO,
+	MODE_MANUAL,
 	MODE_OFF,
 };
 
@@ -48,6 +52,7 @@ struct context {
 APP_TIMER_DEF(led_timer_id);
 APP_TIMER_DEF(sky_movement_timer_id);
 APP_TIMER_DEF(goto_timer_id);
+APP_TIMER_DEF(manual_timer_id);
 
 void app_error_fault_handler(uint32_t id, uint32_t pc, uint32_t info)
 {
@@ -111,7 +116,8 @@ static void sky_movement_timer_handler(void *p_context)
 		nrf_gpio_pin_toggle(RA_STEP);
 		break;
 	case MODE_GOTO:
-		/* In GOTO mode RA motor is controlled by different routine. */
+	case MODE_MANUAL:
+		/* In GOTO and MANUAL mode RA motor is controlled by different routine. */
 		if (ctx.reverse)
 			ctx.dest[0] -= STEP_LENGTH;
 		else
@@ -123,10 +129,15 @@ static void sky_movement_timer_handler(void *p_context)
 enum {
 	DIR_PROGRADE,
 	DIR_RETROGRADE,
+	DIR_OFF,
 };
 
 static void ra_set_dir(int dir)
 {
+	/* disabled motor won't go in any direction */
+	if (dir == DIR_OFF)
+		nrf_gpio_pin_set(RA_EN);
+
 	if (dir == DIR_PROGRADE || (dir == DIR_RETROGRADE && ctx.reverse))
 		nrf_gpio_pin_set(RA_DIR);
 	else
@@ -135,6 +146,9 @@ static void ra_set_dir(int dir)
 
 static void dec_set_dir(int dir)
 {
+	if (dir == DIR_OFF)
+		nrf_gpio_pin_set(DEC_EN);
+
 	if (dir == DIR_PROGRADE || (dir == DIR_RETROGRADE && ctx.reverse))
 		nrf_gpio_pin_set(DEC_DIR);
 	else
@@ -155,17 +169,20 @@ static void set_mode(int mode)
 	case MODE_GOTO:
 		app_timer_stop(goto_timer_id);
 		break;
+	case MODE_MANUAL:
+		app_timer_stop(manual_timer_id);
+		break;
 	}
 
 	switch (mode) {
 	case MODE_TRACKING:
+		NRF_LOG_INFO("\r\nGoing into TRACKING mode.\r\n")
 		/* 1/16 microsteps */
 		nrf_gpio_pin_set(RA_MS1);
 		nrf_gpio_pin_set(RA_MS2);
 		nrf_gpio_pin_set(RA_MS3);
 
 		nrf_gpio_pin_clear(RA_EN);
-		nrf_gpio_pin_clear(RA_DIR);
 
 		/* disable DEC motor */
 		nrf_gpio_pin_set(DEC_EN);
@@ -174,15 +191,42 @@ static void set_mode(int mode)
 
 		break;
 	case MODE_GOTO:
+		NRF_LOG_INFO("\r\nGoing into GOTO mode.\r\n")
+		NRF_LOG_INFO("\r\n pos = [%d, %d], dest = [%d, %d]\r\n",
+				ctx.pos[0], ctx.pos[1], ctx.dest[0], ctx.dest[1])
 		/* full steps for better speed */
 		nrf_gpio_pin_clear(RA_MS1);
 		nrf_gpio_pin_clear(RA_MS2);
 		nrf_gpio_pin_clear(RA_MS3);
 
+		/* both motors enabled */
+		nrf_gpio_pin_clear(DEC_EN);
+		nrf_gpio_pin_clear(RA_EN);
+
 		ret = app_timer_start(goto_timer_id, APP_TIMER_TICKS(GOTO_STEP_MS, TIMER_PRESCALER), NULL);
 		APP_ERROR_CHECK(ret);
 		break;
+	case MODE_MANUAL:
+		NRF_LOG_INFO("\r\nGoing into MANUAL mode.\r\n")
+		/* 1/16 microsteps */
+		nrf_gpio_pin_set(RA_MS1);
+		nrf_gpio_pin_set(RA_MS2);
+		nrf_gpio_pin_set(RA_MS3);
+
+		nrf_gpio_pin_set(DEC_MS1);
+		nrf_gpio_pin_set(DEC_MS2);
+		nrf_gpio_pin_set(DEC_MS3);
+
+		/* both motors enabled */
+		nrf_gpio_pin_clear(DEC_EN);
+		nrf_gpio_pin_clear(RA_EN);
+
+		/* TODO Adjustable speed */
+		ret = app_timer_start(manual_timer_id, APP_TIMER_TICKS(MANUAL_STEP_MS, TIMER_PRESCALER), NULL);
+		APP_ERROR_CHECK(ret);
+		break;
 	case MODE_OFF:
+		NRF_LOG_INFO("\r\nGoing into OFF mode.\r\n")
 		break;
 	}
 
@@ -226,6 +270,16 @@ static void goto_timer_handler(void *p_context)
 		set_mode(MODE_TRACKING);
 }
 
+static void manual_timer_handler(void *p_context)
+{
+	if (ctx.mode != MODE_MANUAL)
+		return;
+
+	/* direction control is handled elsewhere */
+	nrf_gpio_pin_toggle(DEC_STEP);
+	nrf_gpio_pin_toggle(RA_STEP);
+}
+
 static int motor_init()
 {
 	uint32_t ret;
@@ -242,6 +296,10 @@ static int motor_init()
 			goto_timer_handler);
 	APP_ERROR_CHECK(ret);
 
+	ret = app_timer_create(&manual_timer_id, APP_TIMER_MODE_REPEATED,
+			manual_timer_handler);
+	APP_ERROR_CHECK(ret);
+
 	nrf_gpio_pin_dir_set(RA_EN, NRF_GPIO_PIN_DIR_OUTPUT);
 	nrf_gpio_pin_dir_set(RA_DIR, NRF_GPIO_PIN_DIR_OUTPUT);
 	nrf_gpio_pin_dir_set(RA_STEP, NRF_GPIO_PIN_DIR_OUTPUT);
@@ -255,6 +313,8 @@ static int motor_init()
 	nrf_gpio_pin_dir_set(DEC_MS1, NRF_GPIO_PIN_DIR_OUTPUT);
 	nrf_gpio_pin_dir_set(DEC_MS2, NRF_GPIO_PIN_DIR_OUTPUT);
 	nrf_gpio_pin_dir_set(DEC_MS3, NRF_GPIO_PIN_DIR_OUTPUT);
+
+	ctx.mode = MODE_OFF;
 
 	return ret;
 }
@@ -281,7 +341,8 @@ int main(void)
 	timer_init();
 
 	NRF_LOG_INFO("\r\nInitializing motors...\r\n")
-	motor_init();
+	motor_init();	
+	set_mode(MODE_TRACKING);
 
 	/* Starting bluetooth service */
 	ret = ble_advertising_start(BLE_ADV_MODE_FAST);
